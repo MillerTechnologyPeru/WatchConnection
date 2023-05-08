6ebdb36bbf8bd54853541b9e1722b89cfa6b32b8
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-05-07 23:16:19 -0700

    Add `WatchConnectionError`

---
+++ Sources/WatchConnection/Error.swift
@@ -0,0 +1,14 @@
+//
+//  WatchConnectionError.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 5/7/23.
+//
+
+import Foundation
+
+public enum WatchConnectionError: Error {
+    
+    case notSupported
+    case notActive
+}
