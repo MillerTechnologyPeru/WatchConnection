41c0be8a5b4ec9fc8a8bcbfd0f5764e906ef5308
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-05-07 23:57:35 -0700

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -1,6 +1,4 @@
 // swift-tools-version: 5.8
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
@@ -12,18 +10,17 @@ let package = Package(
         .tvOS(.v13),
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "WatchConnection",
-            targets: ["WatchConnection"]),
+            targets: ["WatchConnection"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "WatchConnection"),
         .testTarget(
             name: "WatchConnectionTests",
-            dependencies: ["WatchConnection"]),
+            dependencies: ["WatchConnection"]
+        ),
     ]
 )
